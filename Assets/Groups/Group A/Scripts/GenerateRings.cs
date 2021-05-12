3eefe1247475a6aa2f45c2fe6dde376471f7dcda
7geewe <47978520+7geewe@users.noreply.github.com>
2021-05-12 15:43:02 +0200

    Dinge

--- Assets/Groups/Group A/Scripts/GenerateRings.cs
+++ Assets/Groups/Group A/Scripts/GenerateRings.cs
@@ -10,21 +10,24 @@ public class GenerateRings : MonoBehaviour
     public int numberOfRings = 2;
     public float lengthOfBlock = 2;
     public float yLevel = 0;
+    public float randomCapProb = 0.1f;
+    public float offsetRotation = 5f, offsetRadius = 1f, offsetAngle = 5f, offsetHeight = 0.1f;
 
-    public
     // Start is called before the first frame update
     void Start()
     {
         for(int ring = 1; ring <= numberOfRings; ring++)
         {
             float circumference = (float)(Mathf.PI * 2 * radius * ring);
-            int numberOfBlocks = Mathf.FloorToInt( circumference / lengthOfBlock);
-            for(int block = 1; block <= numberOfBlocks; block++)
+            int numberOfBlocks = Mathf.CeilToInt( circumference / lengthOfBlock);
+            float currentRadius = (float)(radius * ring);
+            for (int block = 1; block <= numberOfBlocks; block++)
             {
-                float currentAngle = 360 / numberOfBlocks * block;
-                float currentRadius = (float)(radius * ring);
-                Vector3 position = PointOnCircle(currentRadius, currentAngle);
-                Instantiate(wallPrefab, position, Quaternion.Euler(0, 360-currentAngle, 0));
+                if (Random.Range(0, 100) <= randomCapProb * 100) continue;
+                float currentAngle = (360f / numberOfBlocks) * block;
+                Vector3 position = PointOnCircle(currentRadius + Random.Range(-offsetRadius, offsetRadius), currentAngle + Random.Range(-offsetRotation, offsetRotation));
+                //Debug.Log("currentAngle: " + currentAngle + "  numberOfBlocks: " + numberOfBlocks + "  block: " + block + "  xPos: " + position.x + "  yPos: " + position.y);
+                Instantiate(wallPrefab, position, Quaternion.Euler(0, 360-currentAngle + Random.Range(-offsetAngle, offsetAngle), 0));
             }
         }
     }
@@ -36,7 +39,7 @@ public class GenerateRings : MonoBehaviour
         float x = (float)(radius * Mathf.Cos(angleInDegrees * Mathf.PI / 180F));
         float y = (float)(radius * Mathf.Sin(angleInDegrees * Mathf.PI / 180F));
 
-        return new Vector3(x, yLevel, y);
+        return new Vector3(x, yLevel + Random.Range(-offsetHeight, offsetHeight), y);
     }
 
 }
