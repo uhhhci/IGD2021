f03504e21d3397e834a449cb86573b2d7cb646a3
Johannes Boczek <johannes.boczek@web.de>
2021-05-12 16:33:46 +0200

    Add cannon prefab

---
+++ Assets/Groups/Group K/Scripts/CannonStandoff/CannonPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CannonPlayer : MonoBehaviour {
+	
+	private GameObject cannon;
+	private GameObject marker;
+	
+	private float pitch, yaw;
+	private Vector2 movement;
+	
+	private float rotationSpeed = 0.05f;
+	
+	void Start() {
+		cannon = transform.Find("Cannon").gameObject;
+		marker = transform.Find("Marker").gameObject;
+	}
+	
+	void Update() {
+		marker.transform.Translate(-movement.x * rotationSpeed, movement.y * rotationSpeed, 0.0f);
+		cannon.transform.LookAt(marker.transform);
+	}
+	
+	private void OnMove(InputValue value) {
+		movement = value.Get<Vector2>();
+	}
+	
+	private void OnMoveDpad(InputValue value) {
+		movement = value.Get<Vector2>();
+	}
+	
+}
