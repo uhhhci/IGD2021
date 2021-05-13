7155e71fa2acc51a22e8a1bd966b846655509b19
unknown <6zapatka@informatik.uni-hamburg.de>
2021-05-13 18:18:48 +0200

    tiles, board and movement
    - added prefab for tiles
    - added child prefabs for different types
    - added component for tile neighborhood
    - added a scene for the boardgame
    - added a component controlling player movements
    - players can only move on tiles

---
+++ Assets/Groups/Group D/Scripts/TileNeighborhood.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileNeighborhood : MonoBehaviour
+{
+    public TileNeighborhood left;
+    public TileNeighborhood right;
+    public TileNeighborhood up;
+    public TileNeighborhood down;
+
+    public Vector3 getPosition() {
+        return this.transform.position;
+    }
+}
