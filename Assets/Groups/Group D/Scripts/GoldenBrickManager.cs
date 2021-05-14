7b2331002ff4277d4f3c62ca7d2ca89b1c67519d
unknown <6zapatka@informatik.uni-hamburg.de>
2021-05-14 17:54:35 +0200

    golden brick spawn
    - "Tile" tag for all tiles except start tile
    - "GoldenBrickManager" can relocate the golden brick
    - golden brick starts on random tile when the game starts

---
+++ Assets/Groups/Group D/Scripts/GoldenBrickManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldenBrickManager : MonoBehaviour
+{
+    public Transform brick;
+
+    public float hoverDistance = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        relocate();
+    }
+
+    public void relocate() {
+        // make sure that the start tile is not tagged/included
+        GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tile");
+
+        Vector3 newPos = tiles[Random.Range(0, tiles.Length)].transform.position;
+        newPos.y += hoverDistance;
+        brick.position = newPos;
+    }
+}
