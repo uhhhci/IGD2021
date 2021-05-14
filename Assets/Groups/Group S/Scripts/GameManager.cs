f20d1220876d2077d0a550df8a7495eed56f482e
Frederico Bormann <github@frederico.info>
2021-05-14 10:25:09 +0200

    Add PartKart View Transition

---
+++ Assets/Groups/Group S/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject BuildingFinishedUI;
+    public Transform CameraTransform;
+    
+    private bool buildingFinished = false;
+    public void FinishBuilding()
+    {
+        if (!buildingFinished)
+        {
+            buildingFinished = true;
+            BuildingFinishedUI.SetActive(true);
+            Invoke(nameof(LoadRacingView), 3f);
+        }
+    }
+
+    private void LoadRacingView()
+    {
+        var position = CameraTransform.position;
+        position = new Vector3(position.x, position.y + 10, position.z);
+        CameraTransform.position = position;
+        BuildingFinishedUI.SetActive(false);
+    }
+}
