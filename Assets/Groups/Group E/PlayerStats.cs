c1768205b1b906911215a84cc7d88d157ea18d27
NoFileX <NoFileX@users.noreply.github.com>
2021-05-15 16:58:37 +0200

    Merge lars

---
+++ Assets/Groups/Group E/PlayerStats.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int rounds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rounds = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
