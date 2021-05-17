68ba90831ac3257bf62bf93704b29bb4da90eb34
7wessel <7wessel@informatik-uni.hamburg.de>
2021-05-17 12:33:13 +0200

    Spieler wird von Auto getroffen und verschwindet. Autos kollidieren auch noch.

---
+++ Assets/Groups/Group H/Scripts/PlayerHit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Vehicle")
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+}
