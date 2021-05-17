08c6edcb38a24a0bb8cf258edfca14b191e56618
Yvonne G <40299887+yvonnegoebel@users.noreply.github.com>
2021-05-17 15:11:03 +0200

    Item destroy & respawn collider
    Items verschwinden bei berührung mit playerwsad und Bodenplatte wird als Collider wahrgenommen.

---
+++ Assets/Scripts/playercollision.cs
@@ -0,0 +1,30 @@
+﻿
+using UnityEngine;
+
+public class playercollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnCollisionEnter(Collision col)
+    {
+
+        if (col.collider.tag == "Respawn")
+        {
+            print("respawn");
+        }
+        
+            
+            
+        
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "ItemCollect")
+        {
+            print("colcol");
+            Destroy(col.gameObject);
+        }
+    }
+
+}
