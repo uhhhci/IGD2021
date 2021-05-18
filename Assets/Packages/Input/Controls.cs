7038bc02c435d7ae8ca4f528ed36d553bd697d5f
Polina <polina.rajko@gmx.de>
2021-05-18 09:41:15 +0200

    Added landscape & fixed light

--- Assets/Packages/Input/Controls.cs
+++ Assets/Packages/Input/Controls.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/Input/Controls.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/Packages/Input/Controls.inputactions'
 
 using System;
 using System.Collections;
