1c1e89b79356b4da7d917e9247494e7e37b314b3
unknown <6zapatka@informatik.uni-hamburg.de>
2021-05-19 01:11:44 +0200

    added tile effects for blue and red tiles
    - blue tiles give 2 credits
    - red tiles take 2 credits away
    - added "bobbing" animation for credits
    - tile effect uses the new bobbing animation
    - no effects for purple and orange tiles so far

--- Assets/Groups/Group D/Scripts/TurnAnimationBrick.cs
+++ Assets/Groups/Group D/Scripts/TurnAnimationBrick.cs
@@ -4,18 +4,43 @@ using UnityEngine;
 
 public class TurnAnimationBrick : MonoBehaviour
 {
+    public float rotationSpeed = -90f;
+    public float bobbingSpeed = 20f;
+    public float boobingAmplitude = 0.2f;
+    private bool bobbing = false;
+    private bool bobbingWasDeactivated = false;
 
-    public float rotationSpeed = 5.0f;
+    private Vector3 initialPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        initialPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.RotateAround(transform.position, transform.up, rotationSpeed * Time.deltaTime);
+
+        if (bobbing) {
+            Vector3 newPos = initialPos;
+            newPos.y += Mathf.Sin(Time.timeSinceLevelLoad * bobbingSpeed) * boobingAmplitude;
+            transform.position = newPos;
+        }
+        else if (bobbingWasDeactivated) {
+            transform.position = initialPos;
+            bobbingWasDeactivated = false;
+        }
+    }
+
+    public void setBobbing(bool value) {
+        if (!bobbing && value) { // if bobbing is turned on
+            initialPos = transform.position;
+        }
+        else if (bobbing && !value) { // if bobbing is turned off 
+            bobbingWasDeactivated = true;
+        }
+        bobbing = value;
     }
 }
--- Assets/Groups/Group D/Scripts/TurnAnimationCredit.cs
+++ Assets/Groups/Group D/Scripts/TurnAnimationCredit.cs
@@ -4,18 +4,43 @@ using UnityEngine;
 
 public class TurnAnimationCredit : MonoBehaviour
 {
+    public float rotationSpeed = 5f;
+    public float bobbingSpeed = 20f;
+    public float boobingAmplitude = 0.2f;
+    private bool bobbing = false;
+    private bool bobbingWasDeactivated = false;
 
-    public float rotationSpeed = -90f;
+    private Vector3 initialPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        initialPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.RotateAround(transform.position, transform.right, rotationSpeed * Time.deltaTime);
+
+        if (bobbing) {
+            Vector3 newPos = initialPos;
+            newPos.y += Mathf.Sin(Time.timeSinceLevelLoad * bobbingSpeed) * boobingAmplitude;
+            transform.position = newPos;
+        }
+        else if (bobbingWasDeactivated) {
+            transform.position = initialPos;
+            bobbingWasDeactivated = false;
+        }
+    }
+
+    public void setBobbing(bool value) {
+        if (!bobbing && value) { // if bobbing is turned on
+            initialPos = transform.position;
+        }
+        else if (bobbing && !value) { // if bobbing is turned off 
+            bobbingWasDeactivated = true;
+        }
+        bobbing = value;
     }
 }
