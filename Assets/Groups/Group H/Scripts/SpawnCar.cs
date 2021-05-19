2cf5d2ac87e10231fd124861ce61cbcc9534d27f
Felix Beese <6beese@informatik.uni-hamburg.de>
2021-05-19 18:21:09 +0200

    Fixed car spawning position and cleaned code

--- Assets/Groups/Group H/Scripts/SpawnCar.cs
+++ Assets/Groups/Group H/Scripts/SpawnCar.cs
@@ -5,32 +5,37 @@ using UnityEngine;
 public class SpawnCar : MonoBehaviour
 {
     public List<GameObject> cars;
-    public int xPos;
-    public int zPos;
-    public float rotation;
-    public int carcount;
-    public int carnumber;
+    public int waitingSeconds = 2;
+
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(carSpawn());
     }
+
     IEnumerator carSpawn()
     {
-        while (carcount < 2)
+        while (true)
         {
-            xPos = Random.Range(-2,2)*2;
-            zPos = Random.Range(-2,2)*2;
-            rotation = Random.Range(0,3)*90;
-            carnumber = Random.Range(0,7);
+            int xPos = 0;
+            int zPos = 0;
+            int lane = Random.Range(-2,2)*2;
+            int direction = Random.Range(0, 3);
+            int rotation = direction * 90;
+            if (direction % 2 == 0)
+            {
+                xPos = lane;
+            }
+            else
+            {
+                zPos = lane;
+            }
+            int carnumber = Random.Range(0,cars.Count-1);
             Quaternion target = Quaternion.Euler(0,rotation,0);
             GameObject newCar = Instantiate(cars[carnumber],new Vector3(xPos,0,zPos),target);
-            carcount =0;
             Destroy(newCar,3);  
-            yield return new WaitForSeconds(2);
+            yield return new WaitForSeconds(waitingSeconds);
             Debug.Log("Super! Auto erstellt!"+" "+xPos+" "+zPos+" "+rotation);
-            
         }
     }
-    
 }
