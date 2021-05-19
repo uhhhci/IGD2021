8e41bfd65efb175ec45917b9d14934992792efd2
JannFreiwald <jann.freiwald@googlemail.com>
2021-05-19 20:59:19 +0200

    Merge Group D

---
+++ Assets/Groups/Group D/Scripts/PlayerAction.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAction : MonoBehaviour
+{
+    public enum Type {
+        END_TURN,
+        BUY_GOLDEN_BRICK,
+        // TODO: add more actions here
+    }
+
+    public Type type;
+    public int requiredAP;
+
+    public void setPosition(Vector3 newPos) {
+        transform.position = newPos;
+    }
+}
