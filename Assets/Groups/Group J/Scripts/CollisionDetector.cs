7b31d5610d1a76e303b2af18e6259ee1c5e901a7
Trayanus <84106367+Trayanus@users.noreply.github.com>
2021-05-19 10:49:19 +0200

    Group J - minimal playable prototype (#13)
    * Add_Minigame_Group_J
    
    * Add Ground
    
    * Add Ground
    
    * Add Ground
    
    * Create Arm.prefab.meta
    
    * 4 Player Collision
    
    * Added Lava and Timer
    
    Lava and Timer
    
    * Revert "Added Lava and Timer"
    
    This reverts commit cd150899892157304cda4e2c929d06e2e4c216ea.
    
    * Added Lava, LifeCounter and Timer
    
    * Removed tags and added boolean for team choice
    
    * Remove edited tagmanager
    
    Co-authored-by: 6yilmaz <6yilmaz@informatik.uni-hamburg.de>
    Co-authored-by: northerndevelopment <florianholtorf@kabelmail.de>

---
+++ Assets/Groups/Group J/Scripts/CollisionDetector.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollisionDetector : MonoBehaviour
+{
+    public AudioClip hitSound;
+    AudioSource audioSource;
+    public GameObject model;
+    [SerializeField]
+    private float durationSeconds;
+    [SerializeField]
+    private float deltaTime;
+    private GameManager gameplayManager;
+    public bool isTeam1 = false;
+    public bool isTeam2 = false;
+
+    void Awake()
+    {
+        gameplayManager = GameObject.FindObjectOfType<GameManager>();
+    }
+    public void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        durationSeconds = 1.5f;
+        deltaTime = 0.15f;
+        PlayerPrefs.SetInt("playerDeaths", 0);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == ("Obstacle"))
+        {
+            StartCoroutine(ActivateInvincibility());
+            gameplayManager.UpdateDeath(isTeam1);
+        }
+    }
+
+    IEnumerator ActivateInvincibility()
+    {
+        Physics.IgnoreLayerCollision(gameObject.layer, 13, true);
+
+        if (hitSound)
+        {
+            audioSource.PlayOneShot(hitSound);
+        }
+
+        for (float i = 0; i < durationSeconds; i += deltaTime)
+        {
+            if (model.transform.localScale == Vector3.one)
+            {
+                ScaleModelTo(Vector3.zero);
+            }
+            else
+            {
+                ScaleModelTo(Vector3.one);
+            }
+
+            yield return new WaitForSeconds(deltaTime);
+        }
+        Physics.IgnoreLayerCollision(gameObject.layer, 13, false);
+        ScaleModelTo(Vector3.one);
+    }
+
+    private void ScaleModelTo(Vector3 scale)
+    {
+        model.transform.localScale = scale;
+    }
+}
---
+++ Assets/Groups/Group J/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int deathCount1 = 0;
+    public int deathCount2 = 0;
+    public Text team1DeathCount;
+    public Text team2DeathCount;
+    void Start()
+    {
+        
+    }
+
+    public void UpdateDeath(bool isTeam1)
+    {
+        if (isTeam1) 
+        {
+            deathCount1++;
+            team1DeathCount.text = "Deaths: " + deathCount1;
+        }
+        else
+        {
+            deathCount2++;
+            team2DeathCount.text = "Deaths: " + deathCount2;
+        }
+        
+           
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
