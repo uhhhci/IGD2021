36970503fef275e27c0033663c72cc98601ed095
17nouri <7nouri@informatikum.uni-hamburg.de>
2021-05-23 15:23:03 +0200

    Added rotation on the spot to items (when not picked up)

---
+++ Assets/Groups/Group R/Scripts/Item.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+
+    public bool isPickedUp = false;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(!isPickedUp){
+            transform.Rotate(new Vector3(0f,2.0f,0f) , Space.World);
+        }
+    }
+}
