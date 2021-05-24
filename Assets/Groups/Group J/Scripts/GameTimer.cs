e3f486ab06f629fd06e55efdddd7fc407ed0a8c0
Trayanus <84106367+Trayanus@users.noreply.github.com>
2021-05-24 19:56:58 +0200

    Added slice drops to timer

--- Assets/Groups/Group J/Scripts/GameTimer.cs
+++ Assets/Groups/Group J/Scripts/GameTimer.cs
@@ -8,11 +8,38 @@ public class GameTimer : MonoBehaviour
     public float timeRemaining = 90;
     public bool timerIsRunning = false;
     public Text timeText;
+    public Transform cylinderSlices;
+    public List<Transform> allChildren = new List<Transform>();
+    private List<bool> allChildrenBoolean = new List<bool>();
+    public float maxTimeUntilPlateDestruction = 10;
+    private float timerPlate = 0;
+    public float timerStartPlateDestruction = 60;
+    public bool randomDestructionTimer = true;
+    private bool nextRandomDestructionTime = true; 
 
     private void Start()
     {
         // Starts the timer automatically
         timerIsRunning = true;
+
+        //Set Timer for plate destruction
+        if (maxTimeUntilPlateDestruction > timeRemaining) {
+            maxTimeUntilPlateDestruction = timeRemaining;
+        }
+        if(timerStartPlateDestruction> timeRemaining)
+        {
+            timerStartPlateDestruction = timeRemaining;
+        }
+        timerPlate = maxTimeUntilPlateDestruction;
+
+        // Get children of cylinderSlices
+        {
+            foreach (Transform child in cylinderSlices)
+            {
+                allChildren.Add(child);
+                allChildrenBoolean.Add(true);
+            }
+        }
     }
 
     void Update()
@@ -23,6 +50,23 @@ public class GameTimer : MonoBehaviour
             {
                 timeRemaining -= Time.deltaTime;
                 DisplayTime(timeRemaining);
+                if (timeRemaining < timerStartPlateDestruction) {
+                    if (timerPlate <0)
+                    {
+                        plateDistruction(0);
+                        timerPlate = maxTimeUntilPlateDestruction;
+                        nextRandomDestructionTime = true;
+
+                    }
+                    else {
+                        if (randomDestructionTimer == true && nextRandomDestructionTime == true)
+                        {
+                                timerPlate = Random.Range(0, maxTimeUntilPlateDestruction);
+                                nextRandomDestructionTime = false;
+                         }
+                            timerPlate -= Time.deltaTime;
+                    }
+            }
             }
             else
             {
@@ -42,4 +86,51 @@ public class GameTimer : MonoBehaviour
 
         timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
+
+    //This method is using itself. The loop will stop looping after 10 iterations!
+    void plateDistruction(int count)
+    {
+            try
+            {
+                 var rdm = Random.Range(0, allChildren.Count);
+            int before = 0;
+            int after = 0;
+            if (rdm != 0 && rdm != 32)
+            {
+                before = rdm - 1;
+                after = rdm + 1;
+            }
+            else
+            {
+                if (rdm == 0)
+                {
+                    before = 32;
+                    after = 1;
+                }
+                else {
+                    before = 31;
+                    after = 0;
+                }
+            }
+
+            if (allChildrenBoolean[rdm] == true & !(allChildrenBoolean[before]==false & allChildrenBoolean[after]== false))
+            {
+                Transform child = allChildren[rdm];
+                allChildrenBoolean[rdm] = false;
+                child.gameObject.SetActive(false);
+            }
+            else {
+                if (count>10)
+                {
+                    plateDistruction(count++);
+                }
+            }
+
+
+        }
+
+            catch { }
+        }
+
+
 }
