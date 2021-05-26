89ec7492e12d1992b5173e74b1edf6321a56abc6
17nouri <7nouri@informatikum.uni-hamburg.de>
2021-05-26 15:10:58 +0200

    Items can now be picked up(needs offset still); added missing item script to batarang prefab

--- Assets/Groups/Group R/Scripts/Item.cs
+++ Assets/Groups/Group R/Scripts/Item.cs
@@ -14,4 +14,24 @@ public class Item : MonoBehaviour
             transform.Rotate(new Vector3(0f,2.0f,0f) , Space.World);
         }
     }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject gameObj = collision.gameObject;
+        if(!isPickedUp && gameObj.tag=="Player"){
+            isPickedUp = true;
+            var tree = new List<int>(){0,1,0,1,0,0,0,0,0,0,2,0,0,1,0,0,2};
+            Transform child = gameObj.transform;
+            foreach (var subtree in tree)
+            {
+                child = child.GetChild(subtree);
+            }
+            transform.SetParent(child);
+            transform.localPosition = new Vector3(0, 0, 0);
+            transform.localRotation = Quaternion.Euler(0,0,0);
+            Rigidbody rb = GetComponent<Rigidbody>();
+            rb.isKinematic = true;
+            rb.useGravity = false;
+        }
+    }
 }
