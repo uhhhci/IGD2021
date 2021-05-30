a5172762c82fc9226a8f87bb1ae7c7f19a7b5528
myrieml <myriemlafdili@gmail.com>
2021-05-30 13:09:44 +0200

    Character selection menu

---
+++ Assets/Groups/Group C - Interconnections/Scripts/CharacterSelection.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelection : MonoBehaviour
+{
+    [SerializeField] private Button previousButton;
+    [SerializeField] private Button nextButton;
+    private int currentCharacter;
+
+    private void Awake()
+    {
+        SelectCharacter(0);
+    }
+
+    private void SelectCharacter(int _index)
+    {
+        previousButton.interactable = (_index != 0);
+        nextButton.interactable = (_index != transform.childCount-1);
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(i == _index);
+        }
+    }
+
+    public void ChangeCharacter(int _change)
+    {
+        currentCharacter += _change;
+        SelectCharacter(currentCharacter);
+    }
+}
