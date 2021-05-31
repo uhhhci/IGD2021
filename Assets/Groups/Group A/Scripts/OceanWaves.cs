56c27ddf1928a5597b273eca85acbbd2be2dddfc
7geewe <47978520+7geewe@users.noreply.github.com>
2021-05-31 20:00:56 +0200

    WAVES - Mr Probz ft. Unity Hub

--- Assets/Groups/Group A/Scripts/OceanWaves.cs
+++ Assets/Groups/Group A/Scripts/OceanWaves.cs
@@ -6,15 +6,17 @@ public class OceanWaves : MonoBehaviour
 {
 
     public GameObject Waves;
-    private Vector3 UL = new Vector3(65f, 12.24871f, -127.5245f);
-    private Vector3 OR = new Vector3(-76f, 12.24871f,78f);
+    private Vector3 UL = new Vector3(30f, -16.45871f, -40f);
+    private Vector3 OR = new Vector3(-40f, -16.45871f, 40f);
     public bool wavesActive= true;
-    public float speed = 0.2f;
+    public float speed;
+    public float noise;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        speed = 0.01f;
+        noise = 0.2f;
         
     }
 
@@ -25,18 +27,16 @@ public class OceanWaves : MonoBehaviour
         if (wavesActive)
         {
        
-            Waves.transform.position = transform.TransformVector( Vector3.Lerp(OR, UL, (Mathf.Sin(speed * Time.time) + 1.0f) / 2.0f));
 
-            
+            //Waves.transform.position = Vector3.Lerp(UL, OR, Mathf.PingPong(Time.time * speed, 1.0f));
+            //Waves.transform.position = Vector3.Lerp(UR, OL, Mathf.PingPong(Time.time * speed, 1.0f));
+            //Vector3 temp = Waves.transform.position;
+            Vector3 temp = Vector3.Lerp(UL, OR, Mathf.PingPong(Time.time * speed, 1.0f));
+            temp = new Vector3(temp.x + 5f * Mathf.Sin(Time.time * noise), temp.y, temp.z + 5f * Mathf.Sin(Time.time * noise));
+            Waves.transform.position = temp;
+
+
         }
-
-
-
-
-
-
-
-
 
 
 
