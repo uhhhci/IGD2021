89a10f602ea8355238c6cb75fc05057808526acd
wittmern <niklas.wittmer@posteo.de>
2021-06-03 21:10:35 +0200

    interconnections stuff

--- Assets/Groups/Group P/Scripts/GameManager.cs
+++ Assets/Groups/Group P/Scripts/GameManager.cs
@@ -1,16 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 
 namespace GroupP {
-    public class GameManager : MonoBehaviour
+    public class GameManager : MiniGame
     {
         private static GameManager _instance;
 
         public static GameManager instance { get { return _instance; }}
 
-        List<GameObject> players = new List<GameObject>();
+        public GameObject player1, player2, player3, player4;
 
         public List<GameObject> songs = new List<GameObject>();
 
@@ -28,6 +29,18 @@ namespace GroupP {
         //float accentpoint;
         // ---------------------------------------
 
+        public override string getDisplayName() {
+        return "Rhythm Game";
+        }
+
+        public override string getSceneName()  {
+            return "RhythmGameScene";
+        }
+
+        public override MiniGameType getMiniGameType() {
+            return MiniGameType.freeForAll;
+        }
+
         void Awake() {
             if(_instance != null && _instance != this) {
                 Destroy(this.gameObject);
@@ -42,6 +55,18 @@ namespace GroupP {
         // Start is called before the first frame update
         void Start()
         {
+            var playerInputs = new List<PlayerInput> { 
+                player1.GetComponent<PlayerInput>(),
+                player2.GetComponent<PlayerInput>(),
+                player3.GetComponent<PlayerInput>(),
+                player4.GetComponent<PlayerInput>()
+            };
+            
+            InputManager.Instance.AssignPlayerInput(playerInputs);
+
+            LoadingManager.Instance.LoadMiniGame(getMiniGameType());
+            
+            
             _instance = this;
             // DANCE
             beatsPerMinute = songs[songIndex].GetComponent<Song>().beatsPerMinute;
@@ -63,6 +88,32 @@ namespace GroupP {
                     //InvokeRepeating("BeatEvent", animOffset * sekPerBeat, sekPerBeat);
                 }
             }
+
+            if(!songs[songIndex].GetComponent<AudioSource>().isPlaying && startPlaying) {
+                int player1Score = player1.GetComponent<Score>().score;
+                int player2Score = player2.GetComponent<Score>().score;
+                int player3Score = player3.GetComponent<Score>().score;
+                int player4Score = player4.GetComponent<Score>().score;
+
+                var scores = new List<(int, int)>();
+                scores.Add((1, player1.GetComponent<Score>().score));
+                scores.Add((2, player2.GetComponent<Score>().score));
+                scores.Add((3, player3.GetComponent<Score>().score));
+                scores.Add((4, player4.GetComponent<Score>().score));
+
+                scores.Sort(delegate ((int, int) p1, (int, int) p2) 
+                {
+                    return p1.Item2.CompareTo(p2.Item2);
+                });
+
+                // TODO check double places
+                int[] first = { scores[3].Item1 };
+                int[] second = { scores[2].Item1 };
+                int[] third = {  scores[1].Item1 };
+                int[] fourth = { scores[0].Item1 };                
+
+                MiniGameFinished(firstPlace: first, secondPlace: second, thirdPlace: third, fourthPlace: fourth);
+            }
         }
 
         public GameObject getSong() {
@@ -70,10 +121,6 @@ namespace GroupP {
         }
 
         //DANCE
-        public void registerPlayer(GameObject player) {
-            players.Add(player);
-        }
-
         void DanceEvent()
         {
             GameEventSystem.current.Hit();
