8a89b0cf2ff9315c9ae06f1f5d802023b1f78a7e
UniquePants <6rutzen@informatik.uni-hamburg.de>
2021-06-04 17:22:24 +0200

    merging

---
+++ Assets/Groups/Group W/Scripts/LegoPaperScissorsMinigame.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LegoPaperScissorsMinigame : MiniGame
+{
+    public override string getDisplayName()
+    {
+        return "LegoPaperScissors";
+    }
+
+    public override MiniGameType getMiniGameType()
+    {
+        return MiniGameType.teamVsTeam;
+    }
+
+    public override string getSceneName()
+    {
+        return "Main";
+    }
+
+    public void MinigameFinished()
+    {
+        // TODO if phase == phase.end (from PhaseHandler), set first int[] first place, ..... 
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     // TODO assignPlayerInput   
+    }
+}
