a0919b07def6980c0d540292dede5da48a83f8f9
Frederico Bormann <github@frederico.info>
2021-06-06 16:34:17 +0200

    Track collected KartParts

---
+++ Assets/Groups/Group S/Scripts/BuildPartCollector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Groups.Group_S
+{
+    public class BuildPartCollector : MonoBehaviour
+    {
+        private HashSet<GameObject> _collected = new HashSet<GameObject>();
+        
+        private void OnCollisionEnter(Collision other)
+        {
+            //if (!other.gameObject.CompareTag("PartKart")) return;
+            _collected.Add(other.gameObject);
+            Debug.Log("ENTER: " + _collected.Count);
+        }
+
+        private void OnCollisionExit(Collision other)
+        {
+            //if (!other.gameObject.CompareTag("PartKart")) return;
+            _collected.Remove(other.gameObject);
+            Debug.Log("EXIT: " + _collected.Count);
+        }
+    }
+}
