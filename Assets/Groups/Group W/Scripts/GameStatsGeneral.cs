383496ebd90df3ed49fd03fd419cb118071e9d59
Polina <polina.rajko@gmx.de>
2021-06-08 13:28:53 +0200

    Frames all added (Bug? with the GeneralStatsPanel

---
+++ Assets/Groups/Group W/Scripts/GameStatsGeneral.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStatsGeneral : MonoBehaviour
+{
+    public string description;
+    public string title;
+
+    TextMesh descriptionTextMesh;
+
+    // will be updated by PhaseHandler
+    public float timeLeft;
+    public PhaseHandler.Phase phase;
+    public PhaseHandler.Team leadingTeam;
+    public int roundCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        descriptionTextMesh = GameObject.Find("DescriptionTextGeneral").GetComponent<TextMesh>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        phase = PhaseHandler.phase;
+        roundCount = PhaseHandler.roundCount;
+        timeLeft = PhaseHandler.timeLeft;
+        leadingTeam = PhaseHandler.leadingTeam;
+        title = $"{phase} Phase";
+
+        descriptionTextMesh.text = $"Total time: {PhaseHandler.passedGameSeconds.ToString("F0")} / {PhaseHandler.maxGameSeconds.ToString("F0")} \nRound {roundCount}; {leadingTeam} is leading.\n{title}\n{description}";
+
+    }
+}
