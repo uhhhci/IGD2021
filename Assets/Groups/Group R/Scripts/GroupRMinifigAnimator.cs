b1035a2a7031fcd3574ecd1d0c5eaabde0ed917e
ThiFry <thilofryen@hotmail.de>
2021-06-08 19:32:57 +0200

    Animation event

---
+++ Assets/Groups/Group R/Scripts/GroupRMinifigAnimator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroupRMinifigAnimator : MonoBehaviour
+{
+    public OurMinifigController Controller;
+
+    public void printSomething()
+    {
+        Debug.Log("Moin");
+    }
+}
