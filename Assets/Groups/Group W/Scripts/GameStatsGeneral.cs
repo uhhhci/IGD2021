4f79798490c7139cf13022cb0168eacc06ba2ad3
Polina <polina.rajko@gmx.de>
2021-06-08 14:33:13 +0200

    GeneralStatsScript

--- Assets/Groups/Group W/Scripts/GameStatsGeneral.cs
+++ Assets/Groups/Group W/Scripts/GameStatsGeneral.cs
@@ -18,19 +18,23 @@ public class GameStatsGeneral : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        descriptionTextMesh = GameObject.Find("DescriptionTextGeneral").GetComponent<TextMesh>();
+        descriptionTextMesh = GameObject.GetComponent<TextMesh>();
     }
 
     // Update is called once per frame
     void Update()
     {
         phase = PhaseHandler.phase;
-        roundCount = PhaseHandler.roundCount;
-        timeLeft = PhaseHandler.timeLeft;
-        leadingTeam = PhaseHandler.leadingTeam;
-        title = $"{phase} Phase";
+            roundCount = PhaseHandler.roundCount;
+            timeLeft = PhaseHandler.timeLeft;
+            leadingTeam = PhaseHandler.leadingTeam;
+            title = $"{phase} Phase";
+            descriptionTextMesh.text = $"Total time: {PhaseHandler.passedGameSeconds.ToString("F0")} / {PhaseHandler.maxGameSeconds.ToString("F0")} \nRound {roundCount}; {leadingTeam} is leading.\n{title}\n{description}";
 
-        descriptionTextMesh.text = $"Total time: {PhaseHandler.passedGameSeconds.ToString("F0")} / {PhaseHandler.maxGameSeconds.ToString("F0")} \nRound {roundCount}; {leadingTeam} is leading.\n{title}\n{description}";
-
+            if (phase == PhaseHandler.Phase.End)
+            {
+                string roundWord = roundCount == 1 ? "round" : "rounds";
+                descriptionTextMesh.text = $"Total time: {PhaseHandler.passedGameSeconds.ToString("F0")}\nGame ended after {roundCount} {roundWord}.\nTeam {leadingTeam} won!";
+        }
     }
 }
