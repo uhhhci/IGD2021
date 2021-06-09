544c1155e956f38c719f9ce75b9df4472d1ae878
JannFreiwald <jann.freiwald@googlemail.com>
2021-06-09 13:52:38 +0200

    Merge Group I

---
+++ Assets/Groups/Group I/Scripts/PlattiGame.cs
@@ -0,0 +1,22 @@
+﻿public class PlattiGame : MiniGame
+{
+
+    public override string getDisplayName()
+    {
+        return "Platti Game";
+    }
+    public override string getSceneName()
+    {
+        return "PlattiGame";
+    }
+
+    public override MiniGameType getMiniGameType()
+    {
+        return MiniGameType.freeForAll;
+    }
+
+    private void Start()
+    {
+        // todo
+    }
+}
