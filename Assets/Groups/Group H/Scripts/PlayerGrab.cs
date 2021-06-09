42a32742ad1ba2a91bc0e895a73ab50e1ae404b4
JannFreiwald <jann.freiwald@googlemail.com>
2021-06-09 14:02:40 +0200

    Merge Group H

---
+++ Assets/Groups/Group H/Scripts/PlayerGrab.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGrab : MonoBehaviour
+{
+
+    public float grabDistance = 1f;
+    public float grabTime = 1f;
+
+    private GameObject grabbedPlayer;
+    private bool grabbing;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        grabbing = false;
+    }
+
+    void FixedUpdate()
+    {
+        if (grabbing)
+        {
+            Grab();
+        }
+    }
+
+    public void Grab()
+    {
+        RaycastHit hit;
+
+        bool hitDetected = Physics.BoxCast(transform.position, transform.localScale, transform.forward, out hit, transform.rotation, grabDistance);
+        if (!grabbing && hitDetected && hit.transform.gameObject.tag == "Player")
+        {
+            grabbedPlayer = hit.collider.gameObject;
+            grabbedPlayer.GetComponent<PlayerGrab>().GetGrabbedBy(transform);
+
+            grabbing = true;
+
+            StartCoroutine("GrabbingTimer");
+        }
+        else if (!grabbing)
+        {
+            StopGrabbing();
+        }
+    }
+
+    private IEnumerator GrabbingTimer()
+    {
+        yield return new WaitForSeconds(grabTime);
+        StopGrabbing();
+    }
+
+    public void StopGrabbing()
+    {
+        grabbing = false;
+        if (grabbedPlayer != null)
+        {
+            grabbedPlayer.GetComponent<PlayerGrab>().StopGettingGrabbed();
+            grabbedPlayer = null;
+        }
+    }
+
+    public void GetGrabbedBy(Transform otherPlayer)
+    {
+        MinifigControllerH minifigController = GetComponent<MinifigControllerH>();
+        minifigController.Follow(otherPlayer);
+    }
+
+    public void StopGettingGrabbed()
+    {
+        MinifigControllerH minifigController = GetComponent<MinifigControllerH>();
+        minifigController.StopFollowing();
+        //minifigController.SetInputEnabled(true);
+    }
+}
--- Assets/Groups/Group H/Scripts/PlayerHit.cs
+++ Assets/Groups/Group H/Scripts/PlayerHit.cs
@@ -1,23 +1,76 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHit : MonoBehaviour
 {
+    // the delay in seconds
+    private float invincibleTime = 2.4f;
+    private float blinkingTime = 0.4f;
+
+    // the audio file for the sound that the figure should make when getting hit
+    public AudioClip explodeAudioClip;
+
+    public CapsuleCollider collider;
+    public GameObject minifigCharacter;
+    private bool isHidden;
+    private bool invincible;
+
+    public Health health; //Calling reduceHealth function from Health script
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        isHidden = false;
+        invincible = false;
     }
 
-    // Update is called once per frame
     void OnCollisionEnter(Collision other)
     {
-        Debug.Log("Treffer");
-        if(other.gameObject.tag == "Vehicle")
+        if (other.gameObject.tag == "Vehicle" && !invincible)
         {
-            Destroy(gameObject);
+            takeDamage();
         }
-        
+    }
+
+    void takeDamage()
+    {
+        if(!invincible)
+        {
+            invincible = true;
+            StartCoroutine(EnableInvincibility(invincibleTime));
+            health.reduceHealth();
+        }
+    }
+
+    IEnumerator EnableInvincibility(float time)
+    {
+        // deactivate collisions
+        //collider.enabled = false;
+        GetComponent<AudioSource>().PlayOneShot(explodeAudioClip);
+
+        // make player blink
+        for (float i = 0; i < time; i += blinkingTime)
+        {
+            ToggleVisibility();
+            yield return new WaitForSeconds(blinkingTime);
+        }
+        // activate collisions
+        //collider.enabled = true;
+        invincible = false;
+    }
+
+    void ToggleVisibility()
+    {
+        if (isHidden)
+        {
+            minifigCharacter.transform.localScale = Vector3.one;
+        }
+        else
+        {
+            minifigCharacter.transform.localScale = Vector3.zero;
+        }
+        isHidden = !isHidden;
     }
 }
