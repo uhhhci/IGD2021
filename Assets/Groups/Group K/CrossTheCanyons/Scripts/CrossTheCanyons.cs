27f1c0b9e9612f7d3dff432ae75fc1ddb4ec9f1e
JannFreiwald <jann.freiwald@googlemail.com>
2021-06-09 14:19:51 +0200

    Merge Group K

---
+++ Assets/Groups/Group K/CrossTheCanyons/Scripts/CrossTheCanyons.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrossTheCanyons : MiniGame
+{
+    
+    public void GameOver(int leftPlayerFinalLevel, int rightPlayerFinalLevel)
+    {
+        int[] firstPlace;
+        int[] secondPlace;
+        if (leftPlayerFinalLevel > rightPlayerFinalLevel)
+        {
+            firstPlace = new int[] {0,1};
+            secondPlace = new int[] {2,3};
+        }
+        else if (rightPlayerFinalLevel > leftPlayerFinalLevel)
+        {
+            firstPlace = new int[] {2,3};
+            secondPlace = new int[] {0,1};
+        }
+        else
+        {
+            firstPlace = new int[] {0,1,2,3};
+            secondPlace = null;
+        }
+        MiniGameFinished(firstPlace, secondPlace, null, null);
+    }
+
+    public override string getDisplayName()
+    {
+        return "Cross the Canyons";
+    }
+    public override string getSceneName()
+    {
+        return "CrossTheCanyons";
+    }
+
+    public override MiniGameType getMiniGameType()
+    {
+        return MiniGameType.teamVsTeam;
+    }
+}
--- Assets/Groups/Group K/CrossTheCanyons/Scripts/GameManagerK.cs
+++ Assets/Groups/Group K/CrossTheCanyons/Scripts/GameManagerK.cs
@@ -1,14 +1,18 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class GameManagerK : MonoBehaviour
 {
+    public CrossTheCanyons crossTheCanyons;
     public MinifigControllerModified minifigure1;
     public MinifigControllerModified minifigure2;
     public LevelManager levelManager;
+    public CountdownTimer timer;
     public GameOverChecker gameOverChecker;
     public BridgeMovement leftBridgeMovement;
     public BridgeMovement rightBridgeMovement;
-    public CountdownTimer timer;
+    public CraneMovement leftCrane;
+    public CraneMovement rightCrane;
     public MoveCamera mainCamera;
 
     //private fields
@@ -17,22 +21,32 @@ public class GameManagerK : MonoBehaviour
     private bool leftPlayerMoveCompleted = false;
     private bool rightPlayerMoveCompleted = false;
 
-    public void ReleaseBridgeSegments()
+    public IEnumerator ReleaseBridgeSegments()
     {
+        leftBridgeMovement.DisconnectFromCrane();
+        rightBridgeMovement.DisconnectFromCrane();
         leftBridge.useGravity = true;
         rightBridge.useGravity = true;
         leftBridgeMovement.enabled = false;
         rightBridgeMovement.enabled = false;
 
+        yield return new WaitForSeconds(1.0f);
+
         (Vector3 leftGoal, Vector3 rightGoal) = levelManager.GetCurrentGoals();
         if (!gameOverChecker.IsLeftPlayerDead())
+        {
+            leftCrane.MoveOutOfScene(true);
             minifigure1.MoveTo(leftGoal, moveDelay: 2.0f, speedMultiplier: 0.2f,
                 onComplete: () => {minifigure1.PlaySpecialAnimation(MinifigControllerModified.SpecialAnimation.Dance, 
                     onSpecialComplete: x => {StartNextLevel(leftPlayer: true);});});
+        }
         if (!gameOverChecker.IsRightPlayerDead())
+        {
+            rightCrane.MoveOutOfScene(false);
             minifigure2.MoveTo(rightGoal, moveDelay: 2.0f, speedMultiplier: 0.2f,
                 onComplete: () => {minifigure2.PlaySpecialAnimation(MinifigControllerModified.SpecialAnimation.Dance, 
-                    onSpecialComplete:x => {StartNextLevel(leftPlayer: false);});});
+                    onSpecialComplete: x => {StartNextLevel(leftPlayer: false);});});
+        }
     }
 
     private void StartNextLevel(bool leftPlayer)
@@ -49,30 +63,36 @@ public class GameManagerK : MonoBehaviour
             rightPlayerMoveCompleted = false;
             mainCamera.MoveForward(levelManager.DistanceBetweenCurrentPlatforms());
             (leftBridge, rightBridge) = levelManager.NextLevel(gameOverChecker.IsLeftPlayerDead(), gameOverChecker.IsRightPlayerDead());
-            timer.ResetTimer();
-
-            if (!gameOverChecker.IsLeftPlayerDead())
-            {
-                leftBridgeMovement.SetBridgeBody(leftBridge);
-                leftBridgeMovement.enabled = true;
-            }
-            if (!gameOverChecker.IsRightPlayerDead())
-            {
-                rightBridgeMovement.SetBridgeBody(rightBridge);
-                rightBridgeMovement.enabled = true;
-            }
+            leftBridgeMovement.SetBridgeBody(leftBridge, gameOverChecker.IsLeftPlayerDead());
+            rightBridgeMovement.SetBridgeBody(rightBridge, gameOverChecker.IsRightPlayerDead());
         }
+    }
+
+    public void BridgeInPosition()
+    {
+        if (!gameOverChecker.IsLeftPlayerDead())
+        {
+            leftBridgeMovement.enabled = true;
+        }
+        if (!gameOverChecker.IsRightPlayerDead())
+        {
+            rightBridgeMovement.enabled = true;
+        }
+        levelManager.ActivateBarriers();
+        timer.StartTimer();
     }
 
     private void Start()
     {
         (leftBridge, rightBridge) = levelManager.Init();
-        leftBridgeMovement.SetBridgeBody(leftBridge);
-        rightBridgeMovement.SetBridgeBody(rightBridge);
+        leftBridgeMovement.SetBridgeBody(leftBridge, false);
+        rightBridgeMovement.SetBridgeBody(rightBridge, false);
     }
 
-    public void GameOver()
+    public void GameOver(int leftPlayerFinalLevel, int rightPlayerFinalLevel)
     {
         Debug.Log("Game Over for both teams!");
+        crossTheCanyons.GameOver(leftPlayerFinalLevel, rightPlayerFinalLevel);
     }
+
 }
