3d125023a413013e9ed5aabc83678eceae85e9a9
lthiell <lukasthiell@icloud.com>
2021-06-16 11:14:29 +0200

    Attack Powerup (yet without white brick mechanic)

---
+++ Assets/Groups/Group E/Scripts/Missile.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Missile : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    public float missileSpeed = 150.0f;
+    public GameObject explosionPrefab;
+
+
+    public void Init()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            Rigidbody playerRb = collision.gameObject.GetComponent<Rigidbody>();
+            CarController playerController = collision.gameObject.GetComponent<CarController>();
+            playerRb.AddExplosionForce(1200f, gameObject.transform.position, 3.0f);
+            playerController.StopCar();
+            AnimateExplosion(gameObject.transform);
+            Destroy();
+        } 
+    }
+
+    private void AnimateExplosion(Transform explosionTransform)
+    {
+        GameObject explosionObject = Instantiate(explosionPrefab, explosionTransform.position, explosionTransform.rotation) as GameObject;
+        explosionObject.SetActive(true);
+        Destroy(explosionObject, 5.0f);
+    }
+
+    public void Shoot()
+    {
+        rb.velocity = transform.forward.normalized * missileSpeed;
+    }
+
+    public void Destroy()
+    {
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/Groups/Group E/Scripts/PowerupAttack.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerupAttack : PowerUp {
+
+    private MissileManager missileManager;
+
+    public PowerupAttack(MissileManager missileManager)
+    {
+        this.missileManager = missileManager;
+    }
+
+    public override string Name { get { return "Attack"; } }
+    public override IEnumerator UsePowerup(GameObject player)
+    {
+        Debug.Log("Name: " + Name);
+
+        CarController controller = player.GetComponent<CarController>();
+        PlayerStats ps = player.GetComponent<PlayerStats>();
+        bool usedBrick = false;
+        if(ps.hasGoldenBrick)
+        {
+            usedBrick = true;
+     
+        }
+        // apply powerup actions
+        Missile missile = missileManager.CreateMissile(player.transform);
+        missile.Shoot();
+
+        ps.UsedPowerup();
+
+        yield return new WaitForSeconds(15);
+
+        // undo actions
+
+        // if not already destroyed
+        if(missile != null)
+        {
+            missile.Destroy();
+        }
+        
+
+        if (usedBrick)
+        {
+          
+        }
+    }
+
+}
