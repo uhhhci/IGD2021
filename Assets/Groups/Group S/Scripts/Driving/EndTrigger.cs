84e4870cfee207757d69d525292176207d69461b
Frederico Bormann <github@frederico.info>
2021-06-22 12:26:17 +0200

    Group S: Add Checkpoints and Finish Line Trigger

---
+++ Assets/Groups/Group S/Scripts/Driving/EndTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Groups.Group_S.Driving
+{
+    public class EndTrigger : MonoBehaviour
+    {
+        public int requiredCheckpoints = 2;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            CheckpointCollector checkpointCollector;
+            if (other.gameObject.TryGetComponent(out checkpointCollector))
+            {
+                if (checkpointCollector.HasPassedAllCheckpoints(requiredCheckpoints))
+                {
+                    Debug.Log("Car successfully finished!");
+                }
+                else
+                {
+                    Debug.Log("Not all checkpoints passed!");
+                }
+            }
+        }
+    }
+}
