2eb8bcfe1132974bcaba6aac24bfe58fb965e0b2
JannFreiwald <jann.freiwald@googlemail.com>
2021-06-25 14:04:46 +0200

    Merge Group W

---
+++ Assets/Groups/Group W/Scripts/Test.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InterconnectionsAssignment : MonoBehaviour
+{
+    // 
+    List<PlayerInput> playerInputs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerInputs = new List<PlayerInput>();
+        foreach (Transform child in transform)
+        {
+            playerInputs.Add(child.GetComponent<PlayerInput>());
+            print($"[BEFORE AssignPlayerInput()] defaultControlScheme: {child.GetComponent<PlayerInput>().defaultControlScheme}");
+        }
+
+        InputManager.Instance.AssignPlayerInput(playerInputs);
+
+         foreach (Transform child in transform)
+         {
+            print($"[AFTER AssignPlayerInput()] defaultControlScheme: {child.GetComponent<PlayerInput>().defaultControlScheme}");
+         }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
