074dff3e26ca081a027f0cefe7c7f1a47cf9d6fb
Vincent Raudszus <vincent.raudszus@t-online.de>
2021-06-26 16:47:14 +0200

    Different models for platform added

--- Assets/Groups/Group K/CrossTheCanyons/Scripts/LevelManager.cs
+++ Assets/Groups/Group K/CrossTheCanyons/Scripts/LevelManager.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-    public GameObject platform;
+    public GameObject platform0;
+    public GameObject platform1;
+    public GameObject platform2;
+    public GameObject platform3;
+    public GameObject platform4;
     public GameObject bridge;
     public GameObject barrier;
     public GameObject ground;
@@ -117,7 +121,7 @@ public class LevelManager : MonoBehaviour
         UpdateBarriers(lvl, leftRightOffset);
         
         float bridgeWidth = 0.4f;
-        float spawnOffset = platform.transform.localScale.x/2 - levels[lvl].bridgeLength/2;
+        float spawnOffset = platform1.transform.localScale.x/2 - levels[lvl].bridgeLength/2;
         Vector3 newBridgePosition = new Vector3(levels[lvl-1].platformCenter.x, levels[lvl-1].platformCenter.y, 0);
         Vector3 leftBridgeOffset = new Vector3(spawnOffset, distanceBridgeToPlatformCenter, -5/2 + bridgeWidth/2) - leftRightOffset + centerOffset;
         Vector3 rightBridgeOffset = new Vector3(spawnOffset, distanceBridgeToPlatformCenter, +5/2 - bridgeWidth/2) + leftRightOffset - centerOffset;
@@ -199,9 +203,47 @@ public class LevelManager : MonoBehaviour
         float nextCenterDistance = Random.Range(minCenterDistance, maxCenterDistance);
         float nextPlatformRotation = Random.Range(-maxRotation, maxRotation);
         Vector3 newPlatformCenter = new Vector3(oldPlatformCenter.x - nextCenterDistance, oldPlatformCenter.y, oldPlatformCenter.z);
-        Vector3 leftRightOffset = new Vector3(0, 0, platform.transform.localScale.z/2);
-        GameObject leftPlatform = Instantiate(platform, newPlatformCenter - leftRightOffset, Quaternion.Euler(0, -nextPlatformRotation, 0));
-        GameObject rightPlatform = Instantiate(platform, newPlatformCenter + leftRightOffset, Quaternion.Euler(0, 180.0f + nextPlatformRotation, 0));
+        Vector3 leftRightOffset = new Vector3(0, 0, platform1.transform.localScale.z/2);
+        int randomness = Random.Range(0,5);
+        GameObject leftPlatform;
+        switch(randomness)
+        {
+            case 1:
+                leftPlatform = Instantiate(platform1, newPlatformCenter - leftRightOffset, Quaternion.Euler(0, -nextPlatformRotation, 0));
+                break;
+            case 2:
+                leftPlatform = Instantiate(platform2, newPlatformCenter - leftRightOffset, Quaternion.Euler(0, -nextPlatformRotation, 0));
+                break;
+            case 3:
+                leftPlatform = Instantiate(platform3, newPlatformCenter - leftRightOffset, Quaternion.Euler(0, -nextPlatformRotation, 0));
+                break;
+            case 4:
+                leftPlatform = Instantiate(platform4, newPlatformCenter - leftRightOffset, Quaternion.Euler(0, -nextPlatformRotation, 0));
+                break;
+            default:
+                leftPlatform = Instantiate(platform0, newPlatformCenter - leftRightOffset, Quaternion.Euler(0, -nextPlatformRotation, 0));
+                break;
+        }
+        randomness = Random.Range(0,5);
+        GameObject rightPlatform;
+        switch(randomness)
+        {
+            case 1:
+                rightPlatform = Instantiate(platform1, newPlatformCenter + leftRightOffset, Quaternion.Euler(0, 180.0f + nextPlatformRotation, 0));
+                break;
+            case 2:
+                rightPlatform = Instantiate(platform2, newPlatformCenter + leftRightOffset, Quaternion.Euler(0, 180.0f + nextPlatformRotation, 0));
+                break;
+            case 3:
+                rightPlatform = Instantiate(platform3, newPlatformCenter + leftRightOffset, Quaternion.Euler(0, 180.0f + nextPlatformRotation, 0));
+                break;
+            case 4:
+                rightPlatform = Instantiate(platform4, newPlatformCenter + leftRightOffset, Quaternion.Euler(0, 180.0f + nextPlatformRotation, 0));
+                break;
+            default:
+                rightPlatform = Instantiate(platform0, newPlatformCenter + leftRightOffset, Quaternion.Euler(0, 180.0f + nextPlatformRotation, 0));
+                break;
+        }
         float newGoalOffset = Random.Range(-0.5f, 1.5f);
         GoalManager leftGoalManager = leftPlatform.GetComponent<GoalManager>();
         GoalManager rightGoalManager = rightPlatform.GetComponent<GoalManager>();
@@ -210,7 +252,7 @@ public class LevelManager : MonoBehaviour
         float additionalBridgeLength = 1.0f / (level + 6.0f) * 6.0f - 0.3f;
         float bridgeLength = (oldLeftGoal - leftGoalManager.GetGoalPosition()).magnitude + additionalBridgeLength - 1;
 
-        levels.Add(new Level(newPlatformCenter, leftGoalManager, rightGoalManager, bridgeLength, nextCenterDistance - platform.transform.localScale.x));
+        levels.Add(new Level(newPlatformCenter, leftGoalManager, rightGoalManager, bridgeLength, nextCenterDistance - platform1.transform.localScale.x));
     }
 
     public (Vector3 leftGoal, Vector3 rightGoal) GetCurrentGoals()
