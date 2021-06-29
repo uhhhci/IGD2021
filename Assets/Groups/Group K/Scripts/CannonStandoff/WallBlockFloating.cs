06a1fbe05b090b7dd58e85c58f4931666f98349a
Johannes Boczek <johannes.boczek@web.de>
2021-06-29 13:53:59 +0200

    Add textures and models
    Add proper brick model
    Add ground texture
    Randomize brick color
    Make wall types more interesting
    Add normal maps

--- Assets/Groups/Group K/Scripts/CannonStandoff/WallBlockFloating.cs
+++ Assets/Groups/Group K/Scripts/CannonStandoff/WallBlockFloating.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WallBlockMoving : MonoBehaviour {
+public class WallBlockFloating : MonoBehaviour {
 	
 	private Rigidbody rigidbody;
 	
