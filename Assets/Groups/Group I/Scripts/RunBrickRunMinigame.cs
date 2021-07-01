5d3d831faf10d03569acf3eea2d52677cbe37e26
James Rucks <jamesleerucks@gmail.com>
2021-07-01 15:45:23 +0200

    Set displayName, gameType and sceneName

---
+++ Assets/Groups/Group I/Scripts/RunBrickRunMinigame.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunBrickRunMinigame : MiniGame
+{
+    public override string getDisplayName()
+    {
+        return "RunBrickRun";
+    }
+
+    public override MiniGameType getMiniGameType()
+    {
+        return MiniGameType.freeForAll;
+    }
+
+    public override string getSceneName()
+    {
+        return "Assets/Groups/Group I/Scenes/RunBrickRun";
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
