acda668feb7b8607771e93d343ec646bfce5761c
Yvonne G <40299887+yvonnegoebel@users.noreply.github.com>
2021-07-04 14:49:34 +0200

    part2

---
+++ Assets/Groups/Group I/Scripts/GameHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        HealthSystemI healthSystem = new HealthSystemI(100);
+        Debug.Log("Health: " + healthSystem.GetHealth2());
+        //healthSystem.Damage(10);
+        //Debug.Log("Health" + healthSystem.GetHealth2());
+        //healthSystem.Heal(10);
+
+    }
+
+   
+}
---
+++ Assets/Groups/Group I/Scripts/HealthSystemI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSystemI
+
+{
+    private int _health;
+    public HealthSystemI(int health)
+    {
+        _health = health;
+
+    }
+
+    public int GetHealth2()
+    {
+        return _health;
+    }
+
+
+    public void Damage(int damageAmount)
+    {
+        _health -= damageAmount; 
+    }
+
+    public void Heal(int healAmount)
+    {
+        _health += healAmount;
+    }
+
+}
