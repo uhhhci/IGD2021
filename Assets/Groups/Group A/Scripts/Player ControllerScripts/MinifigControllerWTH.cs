856e155434dc06be7f55d2b4b6ec25bfecc1063d
7geewe <47978520+7geewe@users.noreply.github.com>
2021-07-06 13:34:11 +0200

    set AI works

--- Assets/Groups/Group A/Scripts/Player ControllerScripts/MinifigControllerWTH.cs
+++ Assets/Groups/Group A/Scripts/Player ControllerScripts/MinifigControllerWTH.cs
@@ -242,6 +242,7 @@ public class MinifigControllerWTH : MonoBehaviour
         if (activate)
         {
             AIStateMachine.SetTrigger("AiIsActive");
+            isAi = activate;
         }
     }
 
@@ -260,7 +261,7 @@ public class MinifigControllerWTH : MonoBehaviour
         }
 
         // Handle input.
-        if (inputEnabled)
+        if (inputEnabled && !isAi)
         {
             // Calculate direct speed and speed.
             var right = Vector3.right;
