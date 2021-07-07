b83c70629536b02802fd387c6b1dc45ddd22dc05
JannFreiwald <jann.freiwald@googlemail.com>
2021-07-07 16:43:00 +0200

    Merge Group I

--- Assets/Groups/Group I/Scripts/Respawn.cs
+++ Assets/Groups/Group I/Scripts/Respawn.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class Respawn : MonoBehaviour
 {
     [SerializeField] private Transform RespawnPointWSAD;
-    
+    public AudioSource playSoundRespawn;
+
     void OnTriggerEnter(Collider other)
     {
         print("Respawn");
         MinifigController otherController = (MinifigController)other.gameObject.GetComponent("MinifigController");
         otherController.TeleportTo(RespawnPointWSAD.transform.position);
+        playSoundRespawn.Play();
+        
     }
 }
--- Assets/Groups/Group I/Scripts/playercollision.cs
+++ Assets/Groups/Group I/Scripts/playercollision.cs
@@ -1,38 +1,111 @@
 ﻿
 using System;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
 
 public class playercollision : MonoBehaviour
 {
     MinifigController player;
+    public AudioSource playSoundGravity;
+    public AudioSource playSoundSpeed;
+    //public AudioSource playSoundHealth;
+    //public int currentHealth = 100;
+
+    private bool finished = false;
+    private bool isAi = false;
 
     private void Start()
     {
         player = (MinifigController)this.GetComponent("MinifigController");
+        //HealtSystem of Player
+        //HealthSystemI healthSystem = new HealthSystemI(100);
+        //currentHealth = 100;
     }
+
+    //public void HealthUp()
+    //{
+    //    HealthSystemI healthSystem = new HealthSystemI(currentHealth);
+    //    healthSystem.Heal(10);
+    //    currentHealth = healthSystem.GetHealth2();
+    //    Debug.Log("Health" + healthSystem.GetHealth2());
+    //}
+
+    //public void HealthDown()
+    //{
+    //    HealthSystemI healthSystem = new HealthSystemI(currentHealth);
+    //    healthSystem.Damage(10);
+    //    currentHealth = healthSystem.GetHealth2();
+    //    Debug.Log("Health" + healthSystem.GetHealth2());
+    //}
 
     void OnTriggerEnter(Collider col)
     {
         GameObject pickup = col.gameObject;
 
-        if (pickup.tag == "ItemCollect")
-        {
-            PickUp(pickup);
-        }
+        //if (pickup.tag == "ItemCollect")
+        //{
+        //    PickUp(pickup);
+        //}
 
         if (pickup.tag == "LowGravity")
         {
             PickUp(pickup);
-            player.gravity /= 2;
+            player.gravity *= 0.9f;
+            playSoundGravity.Play();
         }
 
         if (pickup.tag == "HighSpeed")
         {
             PickUp(pickup);
-            player.maxForwardSpeed *= 2;
+            player.maxForwardSpeed += 0.75f;
+            playSoundSpeed.Play();
         }
 
-        
+        //if (pickup.tag == "PlusLife")
+        //{
+        //    PickUp(pickup);
+        //    HealthUp();
+        //    playSoundHealth.Play();
+        //}
+
+        //if (pickup.tag == "Respawn")
+        //{
+        //    HealthDown();
+            
+        //}
+
+        if (pickup.tag == "Finish")
+        {
+            // Do NOT use PickUp() here!!!
+            finished = true;
+            player.enabled = false;
+
+            Debug.Log(this.name + " finished!");
+        }
+
+    }
+    public float getZPos()
+    {
+        return transform.position.z;
+    }
+
+    public bool hasFinished()
+    {
+        return finished;
+    }
+
+    public void enableAi()
+    {
+        isAi = true;
+
+        MinifigController cont = GetComponent<MinifigController>();
+        NavMeshScript navScript = GetComponent<NavMeshScript>();
+        NavMeshAgent navAgent = GetComponent<NavMeshAgent>();
+
+        cont.enabled = false;
+        navScript.enabled = true;
+        navAgent.enabled = true;
     }
 
     private void PickUp(GameObject pickup)
